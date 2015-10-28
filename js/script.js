b753d9e39a97e97be0825507f9aafa1fbff33f24
GoochW <Gooch.W@etoncollege.org.uk>
2015-10-28 11:02:16 +0000

    Added better creation process

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
+var spotNo = 0;
+var tempSpot;
+
 $(document).on("pageinit", "#home", function() {
-    addSpot(35, 240, "Ash Cloud");
-    addSpot(250, 232, "Main Pipe");
-    addSpot(180, 210, "Side Vent");
-    addSpot(120, 215, "Lava Flow");
+    addSpotText(35, 240, "Ash Cloud");
+    addSpotText(250, 232, "Main Pipe");
+    addSpotText(180, 210, "Side Vent");
+    addSpotText(120, 215, "Lava Flow");
     
-    $(".image").bind("click", function(event) {
+    $(".image").on("click", function(event) {
         var posX = (event.pageX - $(this).offset().left - 15), posY = (event.pageY - $(this).offset().top - 15);
-        console.log("X: " + posX + ", Y: " + posY)
-        addSpot(posY, posX, "SOMETHING");
+        addSpot(posY, posX);
+        spotNo++;
     });
     
-    $("#popup").popup();
+    $("#create-popup-submit-button").on("click", function() {
+        console.log(tempSpot);
+        console.log($("#create-popup-text-input").val())
+        $("#spot-" + tempSpot).attr("popup-text", $("#create-popup-text-input").val());
+        $("#create-popup").popup("close");
+    });
 })
 
-function addSpot(top, left, text) {
+function addSpotText(top, left, text) {
     $(".img-holder").append(
         $("<div />", {"class" : "spot_holder"}).append(
             $("<a />", {
@@ -22,12 +30,41 @@ function addSpot(top, left, text) {
                 "class" : "spot ui-btn ui-nodisc-icon ui-btn-icon-notext ui-corner-all",
                 "data-transition" : "flip",
                 "style" : "top: " + top + "px; left: " + left + "px",
+                "id" : "spot-" + spotNo,
                 "popup-text" : text
             })
         )
     );
     
-    $(".spot").bind("click", function(event) {
+    $("#spot-" + spotNo).bind("click", function(event) {
+        $("#popup").text(event.target.getAttribute("popup-text"));
+        $("#popup").popup();
+    });
+    
+    spotNo++;
+}
+
+function addSpot(top, left) {
+    tempSpot = spotNo
+    console.log(tempSpot);
+    $(".img-holder").append(
+        $("<div />", {"class" : "spot_holder"}).append(
+            $("<a />", {
+                "href" : "#popup",
+                "data-rel" : "popup",
+                "class" : "spot ui-btn ui-nodisc-icon ui-btn-icon-notext ui-corner-all",
+                "data-transition" : "flip",
+                "id" : "spot-" + tempSpot,
+                "style" : "top: " + top + "px; left: " + left + "px"
+            })
+        )
+    );
+    
+    $("#create-popup-text-input").val("");
+    $("#create-popup").popup("open");
+    
+    
+    $(".spot").on("click", function(event) {
         $("#popup").text(event.target.getAttribute("popup-text"));
         $("#popup").popup();
     });
